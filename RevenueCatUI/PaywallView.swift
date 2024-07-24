420b6deef0613765fecb61a1dba233f3ba063002
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2024-07-24 16:52:10 +0300

    Show current plan on Template 7

--- RevenueCatUI/PaywallView.swift
+++ RevenueCatUI/PaywallView.swift
@@ -386,7 +386,7 @@ struct LoadedOfferingPaywallView: View {
                         introEligibility: self.introEligibility)
             .environmentObject(self.introEligibility)
             .environmentObject(self.purchaseHandler)
-            .disabled(self.purchaseHandler.actionInProgress)
+            .isPurchaseActionInProgress(self.purchaseHandler.actionInProgress)
             .onAppear { self.purchaseHandler.trackPaywallImpression(self.createEventData()) }
             .onDisappear { self.purchaseHandler.trackPaywallClose() }
             .onChangeOf(self.purchaseHandler.purchased) { purchased in
@@ -449,7 +449,7 @@ struct LoadedOfferingPaywallView: View {
                 Image(systemName: "xmark")
                     .foregroundColor(color)
             }
-            .disabled(self.purchaseHandler.actionInProgress)
+            .isPurchaseActionInProgress(self.purchaseHandler.actionInProgress)
             .opacity(
                 self.purchaseHandler.actionInProgress
                 ? Constants.purchaseInProgressButtonOpacity
